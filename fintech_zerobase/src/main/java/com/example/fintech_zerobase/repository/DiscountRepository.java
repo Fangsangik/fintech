ed98dc735df290a9aac30d934059cf090b151440
hwang <hwangsangik@gmail.com>
2024-04-22 23:30:01 +0900

    feat: grade에 따른 fee 작용, memberRestfulAPI 작성, delete기능 구현

--- fintech_zerobase/src/main/java/com/example/fintech_zerobase/repository/DiscountRepository.java
+++ fintech_zerobase/src/main/java/com/example/fintech_zerobase/repository/DiscountRepository.java
@@ -4,12 +4,12 @@ import com.example.fintech_zerobase.domain.Member;
 import com.example.fintech_zerobase.type.Grade;
 
 public class DiscountRepository implements DiscountPolicy{
-    private final long discount = 1 / 100;
+    private final long fee = 1 / 100;
 
     @Override
     public long discount(Member member, int price) {
-        if (member.getGrade() == Grade.VIP){
-            return discount;
+        if (member.getGrade() == Grade.BASIC){
+            return fee;
         }
         return 0;
     }
