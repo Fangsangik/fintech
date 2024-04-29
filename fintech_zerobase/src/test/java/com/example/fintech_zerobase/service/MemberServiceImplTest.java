a2300506bd79bb7cd0ae7f1c2da7f48f247922d5
hwang <hwangsangik@gmail.com>
2024-04-29 23:20:41 +0900

    feat: MemberService join test 작성

---
+++ fintech_zerobase/src/test/java/com/example/fintech_zerobase/service/MemberServiceImplTest.java
@@ -0,0 +1,29 @@
+package com.example.fintech_zerobase.service;
+
+import com.example.fintech_zerobase.config.MemberConfig;
+import com.example.fintech_zerobase.domain.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MemberServiceImplTest {
+
+    MemberService memberService;
+
+    @BeforeEach
+    public void beforeEach(){
+        MemberConfig memberConfig = new MemberConfig();
+        memberService = memberConfig.memberService();
+    }
+    @Test
+        void join(){
+        //given
+        Member member = new Member();
+        member.setId(1L);
+        //when
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+        //then
+        Assertions.assertThat(member).isEqualTo(findMember);
+       }
+}
