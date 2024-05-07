a036f14e29bad8ba8183b4e0db4664938779873b
hwang <hwangsangik@gmail.com>
2024-05-07 23:13:46 +0900

    refactor : "일부 기능 추가"

--- fintech_zerobase/src/main/java/com/example/fintech_zerobase/domain/Account.java
+++ fintech_zerobase/src/main/java/com/example/fintech_zerobase/domain/Account.java
@@ -1,5 +1,6 @@
 package com.example.fintech_zerobase.domain;
 
+import com.example.fintech_zerobase.type.AccountStatus;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -17,8 +18,13 @@ public class Account {
 
     @Id @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
+    private String accountNumber;
     private Long balance;
     private LocalDateTime registeredAt;
     private LocalDateTime unregisteredAt;
+
+    private Member member;
+
+    @Enumerated(EnumType.STRING)
+    private AccountStatus accountStatus;
 }
