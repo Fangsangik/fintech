1078fd5847933c062208e40dbbc31e10e07337be
hwang <hwangsangik@gmail.com>
2024-05-16 22:48:08 +0900

    feat : "Custom에외 만들어 보기 아직 해결 못함..."

--- fintech_zerobase/src/main/java/com/example/fintech_zerobase/exception/AccountException.java
+++ fintech_zerobase/src/main/java/com/example/fintech_zerobase/exception/AccountException.java
@@ -1,22 +1,17 @@
 package com.example.fintech_zerobase.exception;
 
 import com.example.fintech_zerobase.type.ErrorCode;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
-@RequiredArgsConstructor
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class AccountException extends RuntimeException {
 
     private ErrorCode errorCode;
     private String errorMessage;
 
-    public AccountException(ErrorCode errorCode){
-        this.errorCode = errorCode;
-        this.errorMessage = errorCode.getText();
-    }
+
 }
--- fintech_zerobase/src/main/java/com/example/fintech_zerobase/type/ErrorCode.java
+++ fintech_zerobase/src/main/java/com/example/fintech_zerobase/type/ErrorCode.java
@@ -5,9 +5,12 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
-@AllArgsConstructor
+@RequiredArgsConstructor
 public enum ErrorCode {
-    USER_NOT_FOUND("사용자가 없습니다.");
-
-    private final String getText;
+    USER_NOT_FOUND("사용자가 없습니다."),
+    USER_UN_MATCH("사용자가 맞지 않습니다."),
+    EMPTY_YOUR_BALANCE("잔고를 비워주세요"),
+    TOO_MUCH_ACCOUNT("생성된 계좌가 너무 많습니다"),
+    TOO_YOUNG_TO_CREATE_BALANCE("너무 어려서 만들 수 없습니다.");
+    private final String getMessage;
 }
