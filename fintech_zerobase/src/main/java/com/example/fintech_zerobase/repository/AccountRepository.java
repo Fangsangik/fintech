e400f0b1957c4b52f8fda375041d5611a1c4bf69
hwang <hwangsangik@gmail.com>
2024-05-16 22:50:56 +0900

    refactor = "Optional로 변경해서 orElse 받기 위해 변경"

--- fintech_zerobase/src/main/java/com/example/fintech_zerobase/repository/AccountRepository.java
+++ fintech_zerobase/src/main/java/com/example/fintech_zerobase/repository/AccountRepository.java
@@ -5,12 +5,13 @@ import com.example.fintech_zerobase.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
 
     Integer countAccount(Member member);
 
-    Account findByInfo(String accountNumber);
+    Optional<Account> findByInfo(String accountNumber);
 
     List<Account> findAllAccount(Member member);
 }
