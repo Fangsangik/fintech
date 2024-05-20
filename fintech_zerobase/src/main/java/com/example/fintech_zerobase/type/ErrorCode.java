88f1c2b5e59a4449c17aefda7b1bdb874d4809a7
sangikHwang <hwangsangik@gamil.com>
2024-05-20 15:53:33 +0900

    feat: transaction관한 errorCode 추가

--- fintech_zerobase/src/main/java/com/example/fintech_zerobase/type/ErrorCode.java
+++ fintech_zerobase/src/main/java/com/example/fintech_zerobase/type/ErrorCode.java
@@ -11,6 +11,11 @@ public enum ErrorCode {
     USER_UN_MATCH("사용자가 맞지 않습니다."),
     EMPTY_YOUR_BALANCE("잔고를 비워주세요"),
     TOO_MUCH_ACCOUNT("생성된 계좌가 너무 많습니다"),
-    TOO_YOUNG_TO_CREATE_BALANCE("너무 어려서 만들 수 없습니다.");
+    TOO_YOUNG_TO_CREATE_BALANCE("너무 어려서 만들 수 없습니다."),
+    ACCOUNT_NOT_FOUND("해당 계좌를 찾을 수 없습니다."),
+    AMOUNT_EXCEED("금액이 초과되었습니다."),
+    TRANSACTION_NOT_VALID("트랜젝션을 사용할 수 없습니다."),
+    CANCEL("실패했습니다"),
+    TOO_OLD_TO_FIND("너무 오래되었습니다.");
     private final String getMessage;
 }
