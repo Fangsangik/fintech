e0e4e3d52df2926f99159cbbb9dbf84d72a551e3
sangikHwang <hwangsangik@gamil.com>
2024-05-20 16:13:00 +0900

    feat: 일부 annotation 추가

--- fintech_zerobase/src/main/java/com/example/fintech_zerobase/repository/DiscountPolicy.java
+++ fintech_zerobase/src/main/java/com/example/fintech_zerobase/repository/DiscountPolicy.java
@@ -1,7 +1,9 @@
 package com.example.fintech_zerobase.repository;
 
 import com.example.fintech_zerobase.domain.Member;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DiscountPolicy {
     long discount(Member member, int price);
 }
--- fintech_zerobase/src/main/java/com/example/fintech_zerobase/service/AccountService.java
+++ fintech_zerobase/src/main/java/com/example/fintech_zerobase/service/AccountService.java
@@ -1,9 +1,11 @@
 package com.example.fintech_zerobase.service;
 
 import com.example.fintech_zerobase.dto.AccountDto;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public interface AccountService {
     AccountDto createAccount(Long id, Long balance);
 
