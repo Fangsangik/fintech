f1d2e0907ab8323d64ae2113eb4083834dca0c9e
sangikHwang <hwangsangik@gamil.com>
2024-05-20 16:14:03 +0900

    feat: 일부 annotation 추가

--- fintech_zerobase/src/main/java/com/example/fintech_zerobase/repository/AccountRepository.java
+++ fintech_zerobase/src/main/java/com/example/fintech_zerobase/repository/AccountRepository.java
@@ -3,10 +3,12 @@ package com.example.fintech_zerobase.repository;
 import com.example.fintech_zerobase.domain.Account;
 import com.example.fintech_zerobase.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
 
     Integer countAccount(Member member);
