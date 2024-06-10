aad52c9f9fa230f7ead87cbf94b652b308feb277
sangikHwang <hwangsangik@gamil.com>
2024-06-10 15:26:48 +0900

    "fromEntity 추가"

--- fintech_zerobase/src/main/java/com/example/fintech_zerobase/dto/AccountDto.java
+++ fintech_zerobase/src/main/java/com/example/fintech_zerobase/dto/AccountDto.java
@@ -1,10 +1,8 @@
 package com.example.fintech_zerobase.dto;
 
+import com.example.fintech_zerobase.domain.Account;
 import com.example.fintech_zerobase.type.AccountStatus;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
@@ -12,14 +10,27 @@ import java.time.LocalDateTime;
 @Setter
 @AllArgsConstructor
 @RequiredArgsConstructor
+@Builder
 public class AccountDto {
     private Long id;
     private String bankName;
     private String password;
     private String balance;
     private String accountNumber;
-    private Long account_amount;
+    private Long accountAmount;
     private AccountStatus accountStatus;
     private LocalDateTime createdAt;
     private LocalDateTime deletedAt;
+
+    public static AccountDto fromEntity(Account account) {
+        return AccountDto.builder()
+                .id(account.getId())
+                .bankName(account.getBankName())
+                .password(account.getPassword())
+                .balance(account.getBalance().toString())
+                .accountNumber(account.getAccountNumber())
+                .accountAmount(account.getAccountAmount())
+                .accountStatus(account.getAccountStatus())
+                .build();
+    }
 }
